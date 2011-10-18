b0eafd6facd6b48e95ecac4b43fb9de0519ddf89
Aditya Mukherjee <aditya.s.mukherjee@gmail.com>
2011-10-18 15:18:36 -0400

    done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,12 +8,24 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                 :content=> "Ruby on Rails Sample App | Home")
+    end
   end
 
-  describe "GET 'cotact'" do
+  describe "GET 'contact'" do
     it "returns http success" do
-      get 'cotact'
+      get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                 :content=> "Ruby on Rails Sample App | Contact")
     end
   end
 
@@ -21,6 +33,13 @@ describe PagesController do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                 :content=> "Ruby on Rails Sample App | About")
     end
   end
 end
